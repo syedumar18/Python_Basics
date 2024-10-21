61660bdd71d6e68390e9bcbe8d325f38e8ec58a0
Syed Umar Nasar <55833962+syedumar18@users.noreply.github.com>
2024-10-21 23:50:31 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,58 @@
+'''
+num_str='18'
+num_int=243
+
+print('Data type before',type(num_str))
+
+num_str = int(num_str)
+
+print('Data type after',type(num_str))
+
+num_sum = num_str + num_int
+print('Calc: ',num_sum)
+'''
+'''
+print('Syed Umar', end=" ")
+print("Nasar")
+
+print('Syed Umar Nasar', 2001, "December", sep=" - ")
+'''
+
+'''
+number = 24.3
+name = 'Umar'
+
+print(name)
+print(number)
+'''
+'''
+a=1
+b=2
+c=3
+print('Value a {}'. format(a),' value b {}'. format(b),' value c {}'. format(c),sep=" - ") '''
+
+'''
+name = input("enter your name: ")
+print("name is ", name)
+print('datatype iski', type(name), 'hai') '''
+'''
+a = 33
+b=22
+a+=5
+print(a) '''
+'''
+a = 33
+d=11
+print(a!=d)'''
+
+'''
+Q=1
+w=11
+print((Q>w) or   (w>Q))'''
+'''
+x=10
+y=4
+print(x&y)'''
+
+name = 'syed umar nasar'
+print('Umar' in name)
